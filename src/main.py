9b11a1b882eef290421b3294aaa7cb2eb5ec47b5
Deepayan Sanyal <deepayan.sanyal93@gmail.com>
2023-03-30 16:54:43 -0500

    save final model

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 """Module implementing the pretraining algorithm"""
 import numpy as np
+import os
 
 import torch
 import torch.utils.data as torchdata
@@ -9,6 +10,9 @@ import datasets
 import models
 import networks
 import utils
+
+TEMP_DIR = "../temp/"
+os.makedirs(TEMP_DIR, exist_ok=True)
 
 
 def get_train_test_acc(model, src_loader, trgt_loader):
@@ -20,8 +24,9 @@ def get_train_test_acc(model, src_loader, trgt_loader):
 
 def main():
     """Main method"""
-    num_epochs = 50
-    steps = 100
+    num_epochs = 5
+    steps = 5
+    b_size = 128
     src_transform_train = transforms.Compose([transforms.ToPILImage(),
                                               transforms.Resize(256),
                                               transforms.RandomResizedCrop(size=224),
@@ -30,7 +35,7 @@ def main():
     src_data_train = datasets.ToyboxDataset(rng=np.random.default_rng(), train=True, transform=src_transform_train,
                                             hypertune=True, num_instances=-1, num_images_per_class=1000,
                                             )
-    src_loader_train = torchdata.DataLoader(src_data_train, batch_size=64, shuffle=True, num_workers=4)
+    src_loader_train = torchdata.DataLoader(src_data_train, batch_size=b_size, shuffle=True, num_workers=4)
     
     src_transform_test = transforms.Compose([transforms.ToPILImage(),
                                              transforms.Resize(224),
@@ -39,7 +44,7 @@ def main():
     
     src_data_test = datasets.ToyboxDataset(rng=np.random.default_rng(), train=True, transform=src_transform_test,
                                            hypertune=True)
-    src_loader_test = torchdata.DataLoader(src_data_test, batch_size=64, shuffle=True, num_workers=4)
+    src_loader_test = torchdata.DataLoader(src_data_test, batch_size=b_size, shuffle=True, num_workers=4)
     
     trgt_transform_train = transforms.Compose([transforms.ToPILImage(),
                                                transforms.Resize(256),
@@ -48,14 +53,14 @@ def main():
                                                transforms.ToTensor(),
                                                transforms.Normalize(mean=datasets.IN12_MEAN, std=datasets.IN12_STD)])
     trgt_data_train = datasets.DatasetIN12SSL(transform=trgt_transform_train, fraction=1.0, hypertune=True)
-    trgt_loader_train = torchdata.DataLoader(trgt_data_train, batch_size=64, shuffle=True, num_workers=4)
+    trgt_loader_train = torchdata.DataLoader(trgt_data_train, batch_size=b_size, shuffle=True, num_workers=4)
     
     trgt_transform_test = transforms.Compose([transforms.ToPILImage(),
                                               transforms.Resize(224),
                                               transforms.ToTensor(),
                                               transforms.Normalize(mean=datasets.IN12_MEAN, std=datasets.IN12_STD)])
     trgt_data_test = datasets.DatasetIN12(train=True, transform=trgt_transform_test, fraction=1.0, hypertune=True)
-    trgt_loader_test = torchdata.DataLoader(trgt_data_test, batch_size=64, shuffle=True, num_workers=4)
+    trgt_loader_test = torchdata.DataLoader(trgt_data_test, batch_size=b_size, shuffle=True, num_workers=4)
     
     # print(utils.online_mean_and_sd(src_loader_train), utils.online_mean_and_sd(src_loader_test))
     # print(utils.online_mean_and_sd(trgt_loader_test))
@@ -81,6 +86,12 @@ def main():
             get_train_test_acc(model=mtl_model, src_loader=src_loader_test, trgt_loader=trgt_loader_test)
 
     get_train_test_acc(model=mtl_model, src_loader=src_loader_test, trgt_loader=trgt_loader_test)
+
+    save_dict = {'backbone': net.backbone.state_dict(),
+                 'classifier_head': net.classifier_head.state_dict(),
+                 'ssl_head': net.ssl_head.state_dict()
+                 }
+    torch.save(save_dict, TEMP_DIR + "final_model.pt")
 
 
 if __name__ == "__main__":
