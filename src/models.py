faaa64971237d1c6531b3f9b801b138ceec86755
sanyald <deepayan.sanyal@vanderbilt.edu>
2023-05-26 15:51:29 -0500

    Adding model for dual supervised learning

--- src/models.py
+++ src/models.py
@@ -8,29 +8,27 @@ import torch.utils.tensorboard as tb
 import utils
 
 
-class ModelFT:
-    """model for finetuning network"""
+class ModelLE:
+    """model for linear eval of network"""
     def __init__(self, network, train_loader, test_loader, logger):
         self.network = network
-        self.train_loader = train_loader
+        self.train_loader = utils.ForeverDataLoader(train_loader)
         self.test_loader = test_loader
         self.logger = logger
         self.network.cuda()
         
-    def train(self, optimizer, scheduler, ep, ep_total):
-        """Finetune the network for 1 epoch"""
+    def train(self, optimizer, scheduler, ep, ep_total, steps):
+        """Train the network for 1 epoch"""
         self.network.set_linear_eval()
         num_batches = 0
         ce_loss_total = 0.0
         src_criterion = nn.CrossEntropyLoss()
-        train_loader_iter = iter(self.train_loader)
-        steps = len(self.train_loader)
         halfway = steps / 2.0
         start_time = time.time()
         for step in range(1, steps + 1):
             optimizer.zero_grad()
     
-            idx, images, labels = next(train_loader_iter)
+            idx, images, labels = self.train_loader.get_next_batch()
             images, labels = images.cuda(), labels.cuda()
             logits = self.network.forward(images)
             loss = src_criterion(logits, labels)
@@ -265,6 +263,106 @@ class SupModel:
             ep, ep_total, steps, steps, optimizer.param_groups[0]['lr'], optimizer.param_groups[1]['lr'],
             ce_loss_total / num_batches, time.time() - start_time))
         
+    def eval(self, loader):
+        """Evaluate the model on the provided dataloader"""
+        n_total = 0
+        n_correct = 0
+        self.network.set_eval()
+        for _, (idxs, images, labels) in enumerate(loader):
+            images, labels = images.cuda(), labels.cuda()
+            with torch.no_grad():
+                logits = self.network.forward(images)
+            top, pred = utils.calc_accuracy(output=logits, target=labels, topk=(1,))
+            n_correct += top[0].item() * pred.shape[0]
+            n_total += pred.shape[0]
+        acc = n_correct / n_total
+        return round(acc, 2)
+
+
+class DualSupModel:
+    """Module implementing the supervised pretraining on source"""
+    
+    def __init__(self, network, source_loader, target_loader, combined_batch, logger):
+        self.network = network
+        self.source_loader = utils.ForeverDataLoader(source_loader)
+        self.target_loader = utils.ForeverDataLoader(target_loader)
+        self.logger = logger
+        self.combined_batch = combined_batch
+        self.network.cuda()
+    
+    def train(self, optimizer, scheduler, steps, ep, ep_total, writer: tb.SummaryWriter):
+        """Train model"""
+        self.network.set_train()
+        num_batches = 0
+        src_ce_loss_total = 0.0
+        trgt_ce_loss_total = 0.0
+        criterion = nn.CrossEntropyLoss()
+        halfway = steps / 2.0
+        start_time = time.time()
+        for step in range(1, steps + 1):
+            optimizer.zero_grad()
+            
+            src_idx, src_images, src_labels = self.source_loader.get_next_batch()
+            src_images, src_labels = src_images.cuda(), src_labels.cuda()
+            trgt_idx, trgt_images, trgt_labels = self.target_loader.get_next_batch()
+            trgt_images, trgt_labels = trgt_images.cuda(), trgt_labels.cuda()
+            
+            if self.combined_batch:
+                comb_images = torch.concat([src_images, trgt_images], dim=0)
+                logits = self.network.forward(comb_images)
+                src_size = src_images.shape[0]
+                src_logits = logits[:src_size]
+                trgt_logits = logits[src_size:]
+            else:
+                src_logits = self.network.forward(src_images)
+                trgt_logits = self.network.forward(trgt_images)
+            
+            src_loss = criterion(src_logits, src_labels)
+            trgt_loss = criterion(trgt_logits, trgt_labels)
+            total_loss = src_loss + trgt_loss
+            
+            total_loss.backward()
+            optimizer.step()
+            if scheduler is not None:
+                scheduler.step()
+            
+            src_ce_loss_total += src_loss.item()
+            trgt_ce_loss_total += trgt_loss.item()
+            num_batches += 1
+            
+            if 0 <= step - halfway < 1:
+                self.logger.info("Ep: {}/{}  Step: {}/{}  BLR: {:.3f}  CLR: {:.3f}  SCE: {:.3f}  TCE: {:.3f}  "
+                                 "T: {:.2f}s"
+                                 .format(ep, ep_total, step, steps,
+                                         optimizer.param_groups[0]['lr'], optimizer.param_groups[1]['lr'],
+                                         src_ce_loss_total / num_batches, trgt_ce_loss_total / num_batches,
+                                         time.time() - start_time))
+            
+            writer.add_scalars(
+                main_tag="training_loss",
+                tag_scalar_dict={
+                    'src_ce_loss_ep': src_ce_loss_total / num_batches,
+                    'src_ce_loss_batch': src_loss.item(),
+                    'trgt_ce_loss_ep': trgt_ce_loss_total / num_batches,
+                    'trgt_ce_loss_batch': trgt_loss.item(),
+                    'total_loss_batch': total_loss.item(),
+                },
+                global_step=(ep - 1) * steps + num_batches,
+            )
+            writer.add_scalars(
+                main_tag="training_lr",
+                tag_scalar_dict={
+                    'bb': optimizer.param_groups[0]['lr'],
+                    'fc': optimizer.param_groups[1]['lr'],
+                },
+                global_step=(ep - 1) * steps + num_batches,
+            )
+        self.logger.info("Ep: {}/{}  Step: {}/{}  BLR: {:.3f}  CLR: {:.3f}  SCE: {:.3f}  TCE: {:.3f}  "
+                         "T: {:.2f}s".format(ep, ep_total, steps, steps,
+                                             optimizer.param_groups[0]['lr'], optimizer.param_groups[1]['lr'],
+                                             src_ce_loss_total / num_batches, trgt_ce_loss_total / num_batches,
+                                             time.time() - start_time))
+    
     def eval(self, loader):
         """Evaluate the model on the provided dataloader"""
         n_total = 0
