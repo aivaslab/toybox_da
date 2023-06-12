4fce9c109dbf0e65af5abf5c63e41475e9b197bc
sanyald <deepayan.sanyal@vanderbilt.edu>
2023-06-12 12:06:54 -0500

    Better exp tracking for trial_1

--- src/dual_learning_trial_1.py
+++ src/dual_learning_trial_1.py
@@ -47,17 +47,24 @@ OUT_DIR = "../out/MTL_1/"
 os.makedirs(TEMP_DIR, exist_ok=True)
 
 
-def get_train_test_acc(model, src_loader, trgt_loader, logger, writer, step):
+def get_train_test_acc(model, src_loader_train, src_loader_test, trgt_loader_train, trgt_loader_test,
+                       logger, writer, step):
     """Get train and test accuracy"""
-    src_acc = model.eval(loader=src_loader)
-    trgt_acc = model.eval(loader=trgt_loader)
-    logger.info("Source Acc: {:.2f}   Target Acc:{:.2f}".format(src_acc, trgt_acc))
+    src_acc_train = model.eval(loader=src_loader_train)
+    src_acc_test = model.eval(loader=src_loader_test)
+    trgt_acc_train = model.eval(loader=trgt_loader_train)
+    trgt_acc_test = model.eval(loader=trgt_loader_test)
+    logger.info("Src Train Acc: {:.2f}  Src Test Acc: {:.2f}  Trgt Train Acc: {:.2f}  Trgt Test Acc:{:.2f}".
+                format(src_acc_train, src_acc_test, trgt_acc_train, trgt_acc_test))
     writer.add_scalars(main_tag="Accuracies",
                        tag_scalar_dict={
-                           'tb_test': src_acc,
-                           'in12_test': trgt_acc,
+                           'tb_train': src_acc_train,
+                           'tb_test': src_acc_test,
+                           'in12_train': trgt_acc_train,
+                           'in12_test': trgt_acc_test,
                        },
                        global_step=step)
+    return src_acc_train, src_acc_test, trgt_acc_train, trgt_acc_test
 
 
 def get_parser():
@@ -89,19 +96,21 @@ def main():
     num_epochs = exp_args['epochs']
     steps = exp_args['iters']
     b_size = exp_args['bsize']
+    hypertune = not exp_args['final']
     
     start_time = datetime.datetime.now()
     save_dir = OUT_DIR + start_time.strftime("%b_%d_%Y_%H_%M") + "/"
     tb_writer = tb.SummaryWriter(log_dir=save_dir)
     logger = utils.create_logger(log_level_str=exp_args['log'], log_file_name=save_dir + "log.txt")
+    
     src_transform_train = transforms.Compose([transforms.ToPILImage(),
                                               transforms.Resize(256),
                                               transforms.RandomResizedCrop(size=224),
                                               transforms.ToTensor(),
                                               transforms.Normalize(mean=datasets.TOYBOX_MEAN, std=datasets.TOYBOX_STD)])
     src_data_train = datasets.ToyboxDataset(rng=np.random.default_rng(), train=True, transform=src_transform_train,
-                                            hypertune=True, num_instances=-1, num_images_per_class=1000,
-                                            )
+                                            hypertune=hypertune, num_instances=-1, num_images_per_class=1000)
+    
     src_loader_train = torchdata.DataLoader(src_data_train, batch_size=b_size, shuffle=True, num_workers=4,
                                             drop_last=True)
     
@@ -109,10 +118,14 @@ def main():
                                              transforms.Resize(224),
                                              transforms.ToTensor(),
                                              transforms.Normalize(mean=datasets.TOYBOX_MEAN, std=datasets.TOYBOX_STD)])
+    src_data_train_eval = datasets.ToyboxDataset(rng=np.random.default_rng(), train=True,
+                                                 transform=src_transform_test, num_images_per_class=3000,
+                                                 num_instances=-1, hypertune=hypertune)
+    src_loader_train_eval = torchdata.DataLoader(src_data_train_eval, batch_size=b_size, shuffle=True, num_workers=4)
     
-    src_data_test = datasets.ToyboxDataset(rng=np.random.default_rng(), train=True, transform=src_transform_test,
-                                           num_images_per_class=3000, hypertune=True)
-    src_loader_test = torchdata.DataLoader(src_data_test, batch_size=b_size, shuffle=True, num_workers=4)
+    src_data_test = datasets.ToyboxDataset(rng=np.random.default_rng(), train=False, transform=src_transform_test,
+                                           num_images_per_class=3000, hypertune=hypertune)
+    src_loader_test = torchdata.DataLoader(src_data_test, batch_size=b_size, shuffle=False, num_workers=4)
     
     color_jitter = transforms.ColorJitter(brightness=0.8, contrast=0.8, hue=0.2, saturation=0.8)
     # gaussian_blur = transforms.GaussianBlur(kernel_size=5, sigma=(0.1, 2.0))
@@ -126,7 +139,7 @@ def main():
                                                transforms.ToTensor(),
                                                transforms.Normalize(mean=datasets.IN12_MEAN, std=datasets.IN12_STD)])
     
-    trgt_data_train = datasets.DatasetIN12SSL(transform=trgt_transform_train, fraction=1.0, hypertune=True)
+    trgt_data_train = datasets.DatasetIN12SSL(transform=trgt_transform_train, fraction=1.0, hypertune=hypertune)
     trgt_loader_train = torchdata.DataLoader(trgt_data_train, batch_size=b_size//2, shuffle=True, num_workers=4,
                                              drop_last=True)
     
@@ -134,7 +147,11 @@ def main():
                                               transforms.Resize(224),
                                               transforms.ToTensor(),
                                               transforms.Normalize(mean=datasets.IN12_MEAN, std=datasets.IN12_STD)])
-    trgt_data_test = datasets.DatasetIN12(train=True, transform=trgt_transform_test, fraction=1.0, hypertune=True)
+    trgt_data_train_eval = datasets.DatasetIN12(train=True, transform=trgt_transform_test, fraction=1.0,
+                                                hypertune=hypertune)
+    trgt_loader_train_eval = torchdata.DataLoader(trgt_data_train_eval, batch_size=b_size, shuffle=True, num_workers=4)
+    
+    trgt_data_test = datasets.DatasetIN12(train=False, transform=trgt_transform_test, fraction=1.0, hypertune=hypertune)
     trgt_loader_test = torchdata.DataLoader(trgt_data_test, batch_size=b_size, shuffle=True, num_workers=4)
     
     # print(utils.online_mean_and_sd(src_loader_train), utils.online_mean_and_sd(src_loader_test))
@@ -157,17 +174,25 @@ def main():
                                                                schedulers=[warmup_scheduler, decay_scheduler],
                                                                milestones=[2 * steps + 1])
     
-    get_train_test_acc(model=mtl_model, src_loader=src_loader_test, trgt_loader=trgt_loader_test, logger=logger,
-                       writer=tb_writer, step=0)
+    get_train_test_acc(model=mtl_model,
+                       src_loader_train=src_loader_train_eval, src_loader_test=src_loader_test,
+                       trgt_loader_train=trgt_loader_train_eval, trgt_loader_test=trgt_loader_test,
+                       logger=logger, writer=tb_writer, step=0)
+    
     for ep in range(1, num_epochs + 1):
         mtl_model.train(optimizer=optimizer, scheduler=combined_scheduler, steps=steps,
                         ep=ep, ep_total=num_epochs, lmbda=5, writer=tb_writer)
         if ep % 20 == 0 and ep != num_epochs:
-            get_train_test_acc(model=mtl_model, src_loader=src_loader_test, trgt_loader=trgt_loader_test, logger=logger,
-                               writer=tb_writer, step=ep * steps)
+            get_train_test_acc(model=mtl_model,
+                               src_loader_train=src_loader_train_eval, src_loader_test=src_loader_test,
+                               trgt_loader_train=trgt_loader_train_eval, trgt_loader_test=trgt_loader_test,
+                               logger=logger, writer=tb_writer, step=ep * steps)
     
-    get_train_test_acc(model=mtl_model, src_loader=src_loader_test, trgt_loader=trgt_loader_test, logger=logger,
-                       writer=tb_writer, step=num_epochs * steps)
+    src_acc_train, src_acc_test, trgt_acc_train, trgt_acc_test = \
+        get_train_test_acc(model=mtl_model,
+                           src_loader_train=src_loader_train_eval, src_loader_test=src_loader_test,
+                           trgt_loader_train=trgt_loader_train_eval, trgt_loader_test=trgt_loader_test,
+                           logger=logger, writer=tb_writer, step=num_epochs * steps)
     
     save_dict = {
         'type': mtl_model.__class__.__name__,
@@ -177,6 +202,17 @@ def main():
         'ssl_head': net.ssl_head.state_dict()
     }
     torch.save(save_dict, save_dir + "final_model.pt")
+
+    exp_args['start_time'] = start_time.strftime("%b %d %Y %H:%M")
+    exp_args['src_train_transform'] = str(src_transform_train)
+    exp_args['trgt_train_transform'] = str(trgt_transform_train)
+    
+    exp_args['tb_train'] = src_acc_train
+    exp_args['tb_test'] = src_acc_test
+    exp_args['in12_train'] = trgt_acc_train
+    exp_args['in12_test'] = trgt_acc_test
+    utils.save_args(path=save_dir, args=exp_args)
+    logger.info("Experimental details and results saved to {}".format(save_dir))
 
 
 if __name__ == "__main__":
