07ad8d03038a10f11f942e4d2caaa0e2ff5c2a7c
sanyald <deepayan.sanyal@vanderbilt.edu>
2023-07-26 16:14:27 -0500

    ccmmd trial 1

---
+++ src/ccmmd.py
@@ -0,0 +1,128 @@
+"""Module for implementing the class-conditional MMD loss"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+import collections
+from typing import Optional, Sequence
+
+torch.set_printoptions(precision=2, linewidth=150)
+
+
+class ClassConditionalMMDLoss(nn.Module):
+    """Class definition for Class Conditional MMD Loss"""
+    def __init__(self, kernels: Sequence[nn.Module]):
+        super(ClassConditionalMMDLoss, self).__init__()
+        self.kernels = kernels
+
+    def forward(self, z_s: torch.Tensor, z_t: torch.Tensor, l_s: torch.Tensor, l_t: torch.Tensor) -> torch.Tensor:
+        """Forward method for MMD loss"""
+        features = torch.cat([z_s, z_t], dim=0)
+        index_matrix = _get_index_matrix(
+            src_labels=list(l_s.cpu().numpy()), trgt_labels=list(l_t.cpu().numpy())).to(z_s.device)
+
+        kernel_matrix = sum([kernel(features) for kernel in self.kernels])  # Add up the matrix of each kernel
+        # Add 2 / (n-1) to make up for the value on the diagonal
+        # to ensure loss is positive in the non-linear version
+        loss = (kernel_matrix * index_matrix).sum()  # + 2. / float(batch_size - 1)
+
+        return loss
+    
+    
+class GaussianKernel(nn.Module):
+    """Gaussian kernel using convolutions"""
+
+    def __init__(self, alpha: Optional[float] = 1.0):
+        super(GaussianKernel, self).__init__()
+        self.alpha = alpha
+
+    def forward_old(self, X: torch.Tensor) -> torch.Tensor:
+        """Forward method for gaussian kernel"""
+        l2_distance_square = ((X.unsqueeze(0) - X.unsqueeze(1)) ** 2).sum(2)
+        sigma_squared = self.alpha * torch.mean(l2_distance_square.detach())
+        return torch.exp(-l2_distance_square / (2 * sigma_squared))
+    
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Forward method for gaussian kernel"""
+
+        x2 = X ** 2
+        x2 = torch.sum(x2, dim=1, keepdim=True).repeat(1, X.shape[0])
+        # print(x2, "x2")
+        X_reshaped = X.unsqueeze(1)
+        # print(X.shape, X_reshaped.shape)
+
+        xp = func.conv1d(input=X_reshaped, weight=X_reshaped).squeeze()
+        # print(xp, "xp")
+        # print(xp.shape, x2.shape)
+        l2_distance_square = func.relu(x2 - 2 * xp + x2.transpose(0, 1))
+
+        sigma_squared = self.alpha * torch.mean(l2_distance_square.detach())
+        return torch.exp(-l2_distance_square / (2 * sigma_squared))
+
+
+def _get_index_matrix(src_labels: list, trgt_labels: list) -> torch.Tensor:
+    """
+    Compute and return the index matrix
+    """
+    src_labels_sz = len(src_labels)
+    trgt_labels_sz = len(trgt_labels)
+    index_matrix = torch.zeros(src_labels_sz + trgt_labels_sz, src_labels_sz + trgt_labels_sz)
+    src_labels_ctr = collections.Counter(src_labels)
+    trgt_labels_ctr = collections.Counter(trgt_labels)
+    
+    for i in range(src_labels_sz):
+        for j in range(src_labels_sz):
+            if src_labels[i] == src_labels[j]:
+                index_matrix[i][j] = 1. / float(src_labels_ctr[src_labels[i]] * src_labels_ctr[src_labels[i]])
+                
+    for i in range(trgt_labels_sz):
+        for j in range(trgt_labels_sz):
+            if trgt_labels[i] == trgt_labels[j]:
+                index_matrix[src_labels_sz + i][src_labels_sz + j] = \
+                    1. / float(trgt_labels_ctr[trgt_labels[i]] * trgt_labels_ctr[trgt_labels[i]])
+                
+    for i in range(src_labels_sz):
+        for j in range(trgt_labels_sz):
+            if src_labels[i] == trgt_labels[j]:
+                index_matrix[i][src_labels_sz + j] = \
+                    -1. / float(src_labels_ctr[src_labels[i]] * trgt_labels_ctr[trgt_labels[j]])
+                index_matrix[src_labels_sz + i][j] = \
+                    -1. / float(src_labels_ctr[src_labels[i]] * trgt_labels_ctr[trgt_labels[j]])
+            
+    # for i in range(batch_size):
+    #     for j in range(batch_size):
+    #         if i != j:
+    #             index_matrix[i][j] = 1. / float(batch_size * (batch_size - 1))
+    #             index_matrix[i + batch_size][j + batch_size] = 1. / float(batch_size * (batch_size - 1))
+    # for i in range(batch_size):
+    #     for j in range(batch_size):
+    #         index_matrix[i][j + batch_size] = -1. / float(batch_size * batch_size)
+    #         index_matrix[i + batch_size][j] = -1. / float(batch_size * batch_size)
+    return index_matrix
+
+
+def test_index_matrix():
+    """Method for testing index matrix"""
+    src_labels = [0, 0, 1, 1, 2, 2, 2, 3]
+    trgt_labels = [0, 0, 1, 1, 1, 2, 2]
+    im = _get_index_matrix(src_labels=src_labels, trgt_labels=trgt_labels)
+    print(im)
+    
+    
+def test_gaussian():
+    activations = torch.rand(size=(64, 512), dtype=torch.float32)
+    # print(activations)
+    print(activations.shape)
+    gaussian_kernel = GaussianKernel(alpha=1)
+    dists = gaussian_kernel.forward_old(activations)
+    dists_new = gaussian_kernel.forward(activations)
+    for i in range(dists.shape[0]):
+        for j in range(dists.shape[1]):
+            try:
+                assert dists[i][j].numpy() - dists_new[i][j].numpy() < 1e-4
+            except AssertionError:
+                print(i, j, dists[i][j].numpy(), dists_new[i][j].numpy())
+    
+
+if __name__ == "__main__":
+    # test_index_matrix()
+    test_gaussian()
--- src/models.py
+++ src/models.py
@@ -8,17 +8,19 @@ import torch.utils.tensorboard as tb
 import torch.nn.functional as func
 
 import utils
+import ccmmd
 
 
 class ModelLE:
     """model for linear eval of network"""
+    
     def __init__(self, network, train_loader, test_loader, logger):
         self.network = network
         self.train_loader = utils.ForeverDataLoader(train_loader)
         self.test_loader = test_loader
         self.logger = logger
         self.network.cuda()
-        
+    
     def train(self, optimizer, scheduler, ep, ep_total, steps):
         """Train the network for 1 epoch"""
         self.network.set_linear_eval()
@@ -29,17 +31,17 @@ class ModelLE:
         start_time = time.time()
         for step in range(1, steps + 1):
             optimizer.zero_grad()
-    
+            
             idx, images, labels = self.train_loader.get_next_batch()
             images, labels = images.cuda(), labels.cuda()
             logits = self.network.forward(images)
             loss = src_criterion(logits, labels)
             loss.backward()
-    
+            
             optimizer.step()
             if scheduler is not None:
                 scheduler.step()
-    
+            
             ce_loss_total += loss.item()
             num_batches += 1
             
@@ -48,12 +50,12 @@ class ModelLE:
                     ep, ep_total, step, steps, optimizer.param_groups[0]['lr'], ce_loss_total / num_batches,
                     time.time() - start_time)
                 )
-
+        
         self.logger.info("Ep: {}/{}  Step: {}/{}  LR: {:.3f}  CE: {:.3f}  T: {:.2f}s".format(
             ep, ep_total, steps, steps, optimizer.param_groups[0]['lr'], ce_loss_total / num_batches,
             time.time() - start_time)
         )
-        
+    
     def eval(self, loader):
         """Evaluate the model on the provided dataloader"""
         n_total = 0
@@ -68,17 +70,18 @@ class ModelLE:
             n_total += pred.shape[0]
         acc = n_correct / n_total
         return round(acc, 2)
-        
-        
+
+
 class MTLModel:
     """Module implementing the MTL method for pretraining the DA model"""
+    
     def __init__(self, network, source_loader, target_loader, logger):
         self.network = network
         self.source_loader = utils.ForeverDataLoader(source_loader)
         self.target_loader = utils.ForeverDataLoader(target_loader)
         self.logger = logger
         self.network.cuda()
-        
+    
     def train(self, optimizer, scheduler, steps, ep, ep_total, lmbda, writer: tb.SummaryWriter):
         """Train model"""
         self.network.set_train()
@@ -89,7 +92,7 @@ class MTLModel:
         trgt_criterion = nn.CrossEntropyLoss()
         halfway = steps / 2.0
         start_time = time.time()
-        for step in range(1, steps+1):
+        for step in range(1, steps + 1):
             optimizer.zero_grad()
             
             src_idx, src_images, src_labels = self.source_loader.get_next_batch()
@@ -114,7 +117,7 @@ class MTLModel:
             ce_loss_total += src_loss.item()
             ssl_loss_total += trgt_loss.item()
             num_batches += 1
-
+            
             writer.add_scalars(
                 main_tag="training_loss",
                 tag_scalar_dict={
@@ -141,8 +144,8 @@ class MTLModel:
                                  "T: {:.2f}s".
                                  format(ep, ep_total, step, steps,
                                         optimizer.param_groups[0]['lr'], optimizer.param_groups[1]['lr'],
-                                        optimizer.param_groups[2]['lr'], ce_loss_total/num_batches,
-                                        ssl_loss_total/num_batches, time.time() - start_time)
+                                        optimizer.param_groups[2]['lr'], ce_loss_total / num_batches,
+                                        ssl_loss_total / num_batches, time.time() - start_time)
                                  )
         self.logger.info("Ep: {}/{} Step:{}/{}  BLR: {:.3f}  CLR: {:.3f}  SLR: {:.3f}  CE: {:.3f}  SSL: {:.3f}"
                          "T: {:.2f}s".
@@ -151,7 +154,7 @@ class MTLModel:
                                 optimizer.param_groups[2]['lr'], ce_loss_total / num_batches,
                                 ssl_loss_total / num_batches, time.time() - start_time)
                          )
-        
+    
     def eval(self, loader):
         """Evaluate the model on the provided dataloader"""
         n_total = 0
@@ -207,7 +210,7 @@ class SSLModel:
                 self.logger.info("Ep: {}/{}  Step: {}/{}  BLR: {:.3f}  SLR: {:.3f}  SSL: {:.3f}  T: {:.2f}s".format(
                     ep, ep_total, step, steps, optimizer.param_groups[0]['lr'], optimizer.param_groups[1]['lr'],
                     ssl_loss_total / num_batches, time.time() - start_time))
-
+            
             writer.add_scalars(
                 main_tag="training_loss",
                 tag_scalar_dict={
@@ -355,7 +358,7 @@ class SupModel:
         self.logger.info("Ep: {}/{}  Step: {}/{}  BLR: {:.3f}  CLR: {:.3f}  CE: {:.3f}  T: {:.2f}s".format(
             ep, ep_total, steps, steps, optimizer.param_groups[0]['lr'], optimizer.param_groups[1]['lr'],
             ce_loss_total / num_batches, time.time() - start_time))
-        
+    
     def eval(self, loader):
         """Evaluate the model on the provided dataloader"""
         n_total = 0
@@ -586,7 +589,137 @@ class DualSupModelWithDomain:
                 dom_n_correct += n_zeros.cpu().numpy()
             else:
                 dom_n_correct += n_ones.cpu().numpy()
-                
+        
         acc = n_correct / n_total
         dom_acc = 100. * dom_n_correct / n_total
         return round(acc, 2), round(dom_acc, 2)
+
+
+class DualSupWithCCMMDModel:
+    """Module implementing the Dual Supervised Model with Jan loss"""
+    
+    def __init__(self, network, source_loader, target_loader, combined_batch, logger):
+        self.network = network
+        self.source_loader = utils.ForeverDataLoader(source_loader)
+        self.target_loader = utils.ForeverDataLoader(target_loader)
+        self.ccmmd_loss = ccmmd.ClassConditionalMMDLoss(
+            kernels=([ccmmd.GaussianKernel(alpha=2 ** k) for k in range(-3, 2)])
+        ).cuda()
+        self.ccmmd_loss.train()
+        self.logger = logger
+        self.combined_batch = combined_batch
+        self.network.cuda()
+    
+    def train(self, optimizer, scheduler, steps, ep, ep_total, writer: tb.SummaryWriter):
+        """Train model"""
+        self.network.set_train()
+        num_batches = 0
+        total_batches = (ep - 1) * steps
+        src_ce_loss_total = 0.0
+        trgt_ce_loss_total = 0.0
+        ccmmd_loss_total = 0.0
+        comb_loss_total = 0.0
+        ce_criterion = nn.CrossEntropyLoss()
+        
+        halfway = steps / 2.0
+        start_time = time.time()
+        for step in range(1, steps + 1):
+            optimizer.zero_grad()
+            p = total_batches / (steps * ep_total)
+            alfa = 2 / (1 + math.exp(-10 * p)) - 1
+            
+            src_idx, src_images, src_labels = self.source_loader.get_next_batch()
+            src_images, src_labels = src_images.cuda(), src_labels.cuda()
+            
+            trgt_idx, trgt_images, trgt_labels = self.target_loader.get_next_batch()
+            trgt_images, trgt_labels = trgt_images.cuda(), trgt_labels.cuda()
+            
+            if self.combined_batch:
+                comb_images = torch.concat([src_images, trgt_images], dim=0)
+                feats, logits = self.network.forward(comb_images)
+                src_size = src_images.shape[0]
+                src_feats, src_logits = feats[:src_size], logits[:src_size]
+                trgt_feats, trgt_logits = feats[src_size:], logits[src_size:]
+            else:
+                src_feats, src_logits = self.network.forward(src_images)
+                trgt_feats, trgt_logits = self.network.forward(trgt_images)
+            
+            src_loss = ce_criterion(src_logits, src_labels)
+            trgt_loss = ce_criterion(trgt_logits, trgt_labels)
+            ccmmd_loss = self.ccmmd_loss(z_s=src_feats, z_t=trgt_feats, l_s=src_labels, l_t=trgt_labels)
+            total_loss = src_loss + trgt_loss + 0.01 * alfa * ccmmd_loss
+            
+            total_loss.backward()
+            optimizer.step()
+            
+            src_ce_loss_total += src_loss.item()
+            trgt_ce_loss_total += trgt_loss.item()
+            ccmmd_loss_total += ccmmd_loss.item()
+            comb_loss_total += total_loss.item()
+            num_batches += 1
+            total_batches += 1
+            
+            if 0 <= step - halfway < 1:
+                self.logger.info("Ep: {}/{}  Step: {}/{}  BLR: {:.4f}  CLR: {:.4f}  SCE: {:.4f}  TCE: {:.4f}  "
+                                 "CCMMD: {:.4f}  Tot: {:.4f}  T: {:.0f}s".format(
+                                    ep, ep_total, step, steps,
+                                    optimizer.param_groups[0]['lr'], optimizer.param_groups[1]['lr'],
+                                    src_ce_loss_total / num_batches, trgt_ce_loss_total / num_batches,
+                                    ccmmd_loss_total / num_batches,
+                                    comb_loss_total / num_batches, time.time() - start_time))
+            
+            writer.add_scalars(
+                main_tag="training_loss",
+                tag_scalar_dict={
+                    'src_ce_loss_ep': src_ce_loss_total / num_batches,
+                    'src_ce_loss_batch': src_loss.item(),
+                    'trgt_ce_loss_ep': trgt_ce_loss_total / num_batches,
+                    'trgt_ce_loss_batch': trgt_loss.item(),
+                    'ccmmd_loss_ep': ccmmd_loss_total / num_batches,
+                    'ccmmd_loss_batch': ccmmd_loss.item(),
+                    'comb_loss_ep': comb_loss_total / num_batches,
+                    'comb_loss_batch': total_loss.item(),
+                },
+                global_step=total_batches,
+            )
+            writer.add_scalars(
+                main_tag="training_lr",
+                tag_scalar_dict={
+                    'bb': optimizer.param_groups[0]['lr'],
+                    'fc': optimizer.param_groups[1]['lr'],
+                },
+                global_step=total_batches,
+            )
+            writer.add_scalars(
+                main_tag="training",
+                tag_scalar_dict={
+                    'p': p,
+                    'lambda': alfa,
+                },
+                global_step=total_batches,
+            )
+            if scheduler is not None:
+                scheduler.step()
+        
+        self.logger.info("Ep: {}/{}  Step: {}/{}  BLR: {:.4f}  CLR: {:.4f}  SCE: {:.4f}  TCE: {:.4f}  CCMMD: {:.4f}  "
+                         "Tot: {:.4f}  T: {:.0f}s".format(
+                            ep, ep_total, steps, steps,
+                            optimizer.param_groups[0]['lr'], optimizer.param_groups[1]['lr'],
+                            src_ce_loss_total / num_batches, trgt_ce_loss_total / num_batches,
+                            ccmmd_loss_total / num_batches, comb_loss_total / num_batches,
+                            time.time() - start_time))
+    
+    def eval(self, loader):
+        """Evaluate the model on the provided dataloader"""
+        n_total = 0
+        n_correct = 0
+        self.network.set_eval()
+        for _, (idxs, images, labels) in enumerate(loader):
+            images, labels = images.cuda(), labels.cuda()
+            with torch.no_grad():
+                _, logits = self.network.forward(images)
+            top, pred = utils.calc_accuracy(output=logits, target=labels, topk=(1,))
+            n_correct += top[0].item() * pred.shape[0]
+            n_total += pred.shape[0]
+        acc = n_correct / n_total
+        return round(acc, 2)
