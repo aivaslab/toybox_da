87e47cc9c16066609393ebf4b4f2abcff8f964b1
deepayansanyal93 <deepayan.sanyal@vanderbilt.edu>
2024-03-26 20:22:18 -0700

    mmd code copied from tllib

---
+++ src/mmd_util.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+from typing import Sequence, Optional
+
+
+class JointMultipleKernelMaximumMeanDiscrepancy(nn.Module):
+
+    def __init__(self, kernels: Sequence[Sequence[nn.Module]], linear: Optional[bool] = True):
+        super(JointMultipleKernelMaximumMeanDiscrepancy, self).__init__()
+        self.kernels = kernels
+        self.index_matrix = None
+        self.linear = linear
+
+        self.thetas = [nn.Identity() for _ in kernels]
+
+    def forward(self, z_s: torch.Tensor, z_t: torch.Tensor) -> torch.Tensor:
+        batch_size = int(z_s[0].size(0))
+        self.index_matrix = _update_index_matrix(batch_size, self.index_matrix, self.linear).to(z_s[0].device)
+
+        kernel_matrix = torch.ones_like(self.index_matrix)
+        for layer_z_s, layer_z_t, layer_kernels, theta in zip(z_s, z_t, self.kernels, self.thetas):
+            # print(layer_kernels)
+            layer_features = torch.cat([layer_z_s, layer_z_t], dim=0)
+            # print(layer_z_s.shape, layer_z_t.shape, layer_features.shape)
+            layer_features = theta(layer_features)
+            feats = sum(
+                [kernel(layer_features) for kernel in layer_kernels])  # Add up the matrix of each kernel
+            # print(feats)
+            kernel_matrix *= feats
+            # print(kernel_matrix)
+
+        # print(kernel_matrix, "kernel_matrix")
+        # Add 2 / (n-1) to make up for the value on the diagonal
+        # to ensure loss is positive in the non-linear version
+        loss = (kernel_matrix * self.index_matrix).sum() + 2. / float(batch_size - 1)
+        return loss
+
+
+def _update_index_matrix(batch_size: int, index_matrix: Optional[torch.Tensor] = None,
+                         linear: Optional[bool] = True) -> torch.Tensor:
+    r"""
+    Update the `index_matrix` which convert `kernel_matrix` to loss.
+    If `index_matrix` is a tensor with shape (2 x batch_size, 2 x batch_size), then return `index_matrix`.
+    Else return a new tensor with shape (2 x batch_size, 2 x batch_size).
+    """
+    # import time
+    # start = time.time()
+    if index_matrix is None or index_matrix.size(0) != batch_size * 2:
+        index_matrix = torch.zeros(2 * batch_size, 2 * batch_size)
+        if linear:
+            for i in range(batch_size):
+                s1, s2 = i, (i + 1) % batch_size
+                t1, t2 = s1 + batch_size, s2 + batch_size
+                index_matrix[s1, s2] = 1. / float(batch_size)
+                index_matrix[t1, t2] = 1. / float(batch_size)
+                index_matrix[s1, t2] = -1. / float(batch_size)
+                index_matrix[s2, t1] = -1. / float(batch_size)
+        else:
+            for i in range(batch_size):
+                for j in range(batch_size):
+                    if i != j:
+                        index_matrix[i][j] = 1. / float(batch_size * (batch_size - 1))
+                        index_matrix[i + batch_size][j + batch_size] = 1. / float(batch_size * (batch_size - 1))
+            for i in range(batch_size):
+                for j in range(batch_size):
+                    index_matrix[i][j + batch_size] = -1. / float(batch_size * batch_size)
+                    index_matrix[i + batch_size][j] = -1. / float(batch_size * batch_size)
+    # print("index time: ", time.time() - start)
+    return index_matrix
+
+
+class GaussianKernel(nn.Module):
+
+    def __init__(self, sigma: Optional[float] = None, track_running_stats: Optional[bool] = True,
+                 alpha: Optional[float] = 1.):
+        super(GaussianKernel, self).__init__()
+        assert track_running_stats or sigma is not None
+        self.sigma_square = torch.tensor(sigma * sigma) if sigma is not None else None
+        self.track_running_stats = track_running_stats
+        self.alpha = alpha
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        # print(X.shape, "gk X")
+        l2_distance_square = ((X.unsqueeze(0) - X.unsqueeze(1)) ** 2).sum(2)
+        # print(l2_distance_square.shape, "gk l2")
+
+        if self.track_running_stats:
+            self.sigma_square = self.alpha * torch.mean(l2_distance_square.detach())
+
+        return torch.exp(-l2_distance_square / (2 * self.sigma_square))
