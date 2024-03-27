4240dc99d05c2d4332b82901ba32af280266a397
deepayansanyal93 <deepayan.sanyal@vanderbilt.edu>
2024-03-26 20:24:17 -0700

    save best model during training jan

--- src/jan_main.py
+++ src/jan_main.py
@@ -67,12 +67,107 @@ def get_parser():
     return vars(parser.parse_args())
 
 
+def eval_model(exp_args):
+    """Eval model"""
+    hypertune = not exp_args['final']
+    num_instances = exp_args['instances']
+    num_images_per_class = exp_args['images']
+    b_size = exp_args['bsize']
+    n_workers = exp_args['workers']
+    logger = utils.create_logger(log_level_str='info', log_file_name="", no_save=True)
+
+    prob = 0.2
+    color_transforms = [transforms.RandomApply([transforms.ColorJitter(brightness=0.2)], p=prob),
+                        transforms.RandomApply([transforms.ColorJitter(hue=0.2)], p=prob),
+                        transforms.RandomApply([transforms.ColorJitter(saturation=0.2)], p=prob),
+                        transforms.RandomApply([transforms.ColorJitter(contrast=0.2)], p=prob),
+                        transforms.RandomEqualize(p=prob),
+                        transforms.RandomPosterize(bits=4, p=prob),
+                        transforms.RandomAutocontrast(p=prob)
+                        ]
+    src_transform_train = transforms.Compose([transforms.ToPILImage(),
+                                              transforms.Resize((256, 256)),
+                                              transforms.RandomResizedCrop(size=224, scale=(0.5, 1.0),
+                                                                           interpolation=
+                                                                           transforms.InterpolationMode.BICUBIC),
+                                              transforms.RandomOrder(color_transforms),
+                                              transforms.RandomHorizontalFlip(),
+                                              transforms.ToTensor(),
+                                              transforms.Normalize(mean=datasets.TOYBOX_MEAN, std=datasets.TOYBOX_STD),
+                                              transforms.RandomErasing(p=0.5)
+                                              ])
+
+    src_data_train = datasets.ToyboxDataset(rng=np.random.default_rng(exp_args['seed']), train=True,
+                                            transform=src_transform_train,
+                                            hypertune=hypertune, num_instances=num_instances,
+                                            num_images_per_class=num_images_per_class,
+                                            )
+    logger.debug(f"Source dataset: {src_data_train}  Size: {len(src_data_train)}")
+    src_loader_train = torchdata.DataLoader(src_data_train, batch_size=b_size, shuffle=True, num_workers=n_workers,
+                                            drop_last=True)
+
+    src_transform_test = transforms.Compose([transforms.ToPILImage(),
+                                             transforms.Resize((224, 224)),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize(mean=datasets.TOYBOX_MEAN, std=datasets.TOYBOX_STD)])
+
+    src_data_test = datasets.ToyboxDataset(rng=np.random.default_rng(), train=False, transform=src_transform_test,
+                                           hypertune=hypertune)
+    src_loader_test = torchdata.DataLoader(src_data_test, batch_size=b_size, shuffle=False, num_workers=n_workers)
+
+    trgt_transform_train = transforms.Compose([transforms.ToPILImage(),
+                                               transforms.Resize((256, 256)),
+                                               transforms.RandomResizedCrop(size=224, scale=(0.5, 1.0),
+                                                                            interpolation=
+                                                                            transforms.InterpolationMode.BICUBIC),
+                                               transforms.RandomOrder(color_transforms),
+                                               transforms.RandomHorizontalFlip(),
+                                               transforms.ToTensor(),
+                                               transforms.Normalize(mean=datasets.IN12_MEAN, std=datasets.IN12_STD),
+                                               transforms.RandomErasing(p=0.5)
+                                               ])
+    trgt_data_train = datasets.DatasetIN12(train=True, transform=trgt_transform_train, fraction=1.0,
+                                           hypertune=hypertune)
+    trgt_loader_train = torchdata.DataLoader(trgt_data_train, batch_size=b_size, shuffle=True, num_workers=n_workers,
+                                             drop_last=True)
+
+    trgt_transform_test = transforms.Compose([transforms.ToPILImage(),
+                                              transforms.Resize((224, 224)),
+                                              transforms.ToTensor(),
+                                              transforms.Normalize(mean=datasets.IN12_MEAN, std=datasets.IN12_STD)])
+    trgt_data_test = datasets.DatasetIN12(train=False, transform=trgt_transform_test, fraction=1.0, hypertune=hypertune)
+    trgt_loader_test = torchdata.DataLoader(trgt_data_test, batch_size=b_size, shuffle=False, num_workers=n_workers)
+
+    load_file_path = exp_args['load_path'] + "best_model.pt"
+    assert os.path.isfile(load_file_path)
+    load_file = torch.load(load_file_path)
+    assert load_file['type'] == networks_da.ResNet18JAN.__name__
+    logger.info(f"Loading model weights from {load_file_path} ({load_file['type']})")
+    bb_wts = load_file['backbone']
+    btlnk_wts = load_file['bottleneck']
+    dropout = load_file['dropout']
+    cl_wts = load_file['classifier']
+    net = networks_da.ResNet18JAN(num_classes=12, backbone_weights=bb_wts, bottleneck_weights=btlnk_wts,
+                                  classifier_weights=cl_wts, dropout=dropout)
+
+    model = models_da.JANModel(network=net, source_loader=src_loader_train, target_loader=trgt_loader_train,
+                               logger=logger, combined_batch=True, use_amp=exp_args['amp'], no_save=True)
+
+    model.calc_val_loss(ep=0, steps=0, writer=None, loaders=[src_loader_test, trgt_loader_test],
+                        loader_names=['tb_test', 'in12_test'])
+    get_train_test_acc(model=model, src_train_loader=src_loader_train, src_test_loader=src_loader_test,
+                       trgt_loader=trgt_loader_test, writer=None, step=0, logger=logger, no_save=True)
+
+
+
 def main():
     """Main method"""
-    
     exp_args = get_parser()
     exp_args['seed'] = None if exp_args['seed'] == -1 else exp_args['seed']
     num_epochs = exp_args['epochs']
+    if num_epochs == 0:
+        eval_model(exp_args=exp_args)
+        return
     steps = exp_args['iters']
     b_size = exp_args['bsize']
     n_workers = exp_args['workers']
@@ -83,6 +178,8 @@ def main():
     dropout = exp_args['dropout']
     no_save = exp_args['no_save']
     amp = exp_args['amp']
+    if amp:
+        torch.set_float32_matmul_precision('high')
     
     start_time = datetime.datetime.now()
     tb_path = OUT_DIR + "TB_IN12/" + "exp_" + start_time.strftime("%b_%d_%Y_%H_%M") + "/"
@@ -160,6 +257,7 @@ def main():
         logger.info(f"Loading model weights from {load_file_path} ({load_file['type']})")
         bb_wts = load_file['backbone']
         btlnk_wts = load_file['bottleneck'] if 'bottleneck' in load_file.keys() else None
+        dropout = load_file['dropout'] if 'dropout' in load_file.keys() else dropout
         cl_wts = load_file['classifier'] if (btlnk_wts is not None and 'classifier' in load_file.keys()) else None
         net = networks_da.ResNet18JAN(num_classes=12, backbone_weights=bb_wts, bottleneck_weights=btlnk_wts,
                                       classifier_weights=cl_wts, dropout=dropout)
@@ -168,36 +266,48 @@ def main():
 
     model = models_da.JANModel(network=net, source_loader=src_loader_train, target_loader=trgt_loader_train,
                                logger=logger, combined_batch=combined_batch, use_amp=amp, no_save=no_save)
-    
+
     bb_lr_wt = 0.1 if (exp_args['pretrained'] or
                        (exp_args['load_path'] != "" and os.path.isdir(exp_args['load_path']))) \
         else 1.0
-    
-    optimizer = torch.optim.SGD(net.backbone.parameters(), lr=bb_lr_wt * exp_args['lr'], weight_decay=exp_args['wd'])
-    optimizer.add_param_group({'params': net.bottleneck.parameters(), 'lr': exp_args['lr']})
-    optimizer.add_param_group({'params': net.classifier_head.parameters(), 'lr': exp_args['lr']})
-    
+
+    optimizer = torch.optim.SGD(net.backbone.parameters(), lr=bb_lr_wt * exp_args['lr'], weight_decay=exp_args['wd'],
+                                momentum=0.9, nesterov=True)
+    optimizer.add_param_group({'params': net.bottleneck.parameters(), 'lr': exp_args['lr'], 'wd': exp_args['wd']})
+    optimizer.add_param_group({'params': net.classifier_head.parameters(), 'lr': exp_args['lr'], 'wd': exp_args['wd']})
+
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer,
                                                      lambda x: (1. + 10 * float(x / (num_epochs * steps))) ** (-0.75))
-    
+
     get_train_test_acc(model=model, src_train_loader=src_loader_train,
                        src_test_loader=src_loader_test, trgt_loader=trgt_loader_test,
                        writer=tb_writer, step=0, logger=logger, no_save=no_save)
 
-    model.calc_val_loss(ep=0, steps=steps, writer=tb_writer, loaders=[src_loader_test, trgt_loader_test],
-                        loader_names=['tb_test', 'in12_test'])
-    
+    best_tb_val_loss = float("inf")
+
+    val_losses = model.calc_val_loss(ep=0, steps=steps, writer=tb_writer, loaders=[src_loader_test, trgt_loader_test],
+                                     loader_names=['tb_test', 'in12_test'])
+
+    if val_losses[0] <= best_tb_val_loss:
+        best_tb_val_loss = val_losses[0]
+        if not no_save:
+            net.save_model(fpath=tb_path+"best_model.pt")
+
     for ep in range(1, num_epochs + 1):
         model.train(optimizer=optimizer, scheduler=lr_scheduler, steps=steps,
                     ep=ep, ep_total=num_epochs, writer=tb_writer)
         if ep % 5 == 0:
-            model.calc_val_loss(ep=ep, steps=steps, writer=tb_writer, loaders=[src_loader_test, trgt_loader_test],
-                                loader_names=['tb_test', 'in12_test'])
-        if ep % 20 == 0 and ep != num_epochs:
+            val_losses = model.calc_val_loss(ep=ep, steps=steps, writer=tb_writer,
+                                             loaders=[src_loader_test, trgt_loader_test],
+                                             loader_names=['tb_test', 'in12_test'])
+            if val_losses[0] <= best_tb_val_loss:
+                best_tb_val_loss = val_losses[0]
+                if not no_save:
+                    net.save_model(fpath=tb_path + "best_model.pt")
+        if ep % 5 == 0 and ep != num_epochs:
             get_train_test_acc(model=model, src_train_loader=src_loader_train,
                                src_test_loader=src_loader_test, trgt_loader=trgt_loader_test,
                                writer=tb_writer, step=ep * steps, logger=logger, no_save=no_save)
-    
     src_tr_acc, src_te_acc, trgt_acc = get_train_test_acc(model=model, src_train_loader=src_loader_train,
                                                           src_test_loader=src_loader_test,
                                                           trgt_loader=trgt_loader_test,
@@ -208,6 +318,7 @@ def main():
         tb_writer.close()
         save_dict = {
             'type': net.__class__.__name__,
+            'dropout': net.dropout,
             'backbone': net.backbone.model.state_dict(),
             'bottleneck': net.bottleneck.state_dict(),
             'classifier': net.classifier_head.state_dict(),
@@ -222,6 +333,8 @@ def main():
         utils.save_args(path=tb_path, args=exp_args)
         logger.info("Experimental details and results saved to {}".format(tb_path))
 
+        exp_args['load_path'] = tb_path
+        eval_model(exp_args)
 
 if __name__ == "__main__":
     main()
