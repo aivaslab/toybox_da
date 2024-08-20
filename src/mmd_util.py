28b2b41ade79744e7852c33e635cf4bbaf0f5ca8
deepayansanyal93 <deepayan.sanyal@vanderbilt.edu>
2024-08-20 16:45:31 -0700

    faster mask loading using pytorch

--- src/mmd_util.py
+++ src/mmd_util.py
@@ -15,7 +15,7 @@ class JointMultipleKernelMaximumMeanDiscrepancy(nn.Module):
 
     def forward(self, z_s: torch.Tensor, z_t: torch.Tensor) -> torch.Tensor:
         batch_size = int(z_s[0].size(0))
-        self.index_matrix = _update_index_matrix(batch_size, self.index_matrix, self.linear).to(z_s[0].device)
+        self.index_matrix = _update_index_matrix_2(batch_size, self.index_matrix, self.linear).to(z_s[0].device)
 
         kernel_matrix = torch.ones_like(self.index_matrix)
         for layer_z_s, layer_z_t, layer_kernels, theta in zip(z_s, z_t, self.kernels, self.thetas):
@@ -65,6 +65,35 @@ def _update_index_matrix(batch_size: int, index_matrix: Optional[torch.Tensor] =
                 for j in range(batch_size):
                     index_matrix[i][j + batch_size] = -1. / float(batch_size * batch_size)
                     index_matrix[i + batch_size][j] = -1. / float(batch_size * batch_size)
+    # print("index time: ", time.time() - start)
+    return index_matrix
+
+
+def _update_index_matrix_2(batch_size: int, index_matrix: Optional[torch.Tensor] = None,
+                           linear: Optional[bool] = True) -> torch.Tensor:
+    r"""
+    Update the `index_matrix` which convert `kernel_matrix` to loss.
+    If `index_matrix` is a tensor with shape (2 x batch_size, 2 x batch_size), then return `index_matrix`.
+    Else return a new tensor with shape (2 x batch_size, 2 x batch_size).
+    """
+    # import time
+    # start = time.time()
+    if index_matrix is None or index_matrix.size(0) != batch_size * 2:
+        index_matrix = torch.zeros(2 * batch_size, 2 * batch_size)
+        if linear:
+            for i in range(batch_size):
+                s1, s2 = i, (i + 1) % batch_size
+                t1, t2 = s1 + batch_size, s2 + batch_size
+                index_matrix[s1, s2] = 1. / float(batch_size)
+                index_matrix[t1, t2] = 1. / float(batch_size)
+                index_matrix[s1, t2] = -1. / float(batch_size)
+                index_matrix[s2, t1] = -1. / float(batch_size)
+        else:
+            x = 1. / float(batch_size * (batch_size - 1)) * (1 - torch.eye(batch_size))
+            y = -1. / float(batch_size * batch_size) * torch.ones(batch_size, batch_size)
+            xy, yx = torch.cat((x, y), dim=1), torch.cat((y, x), dim=1)
+            index_matrix = torch.cat([xy, yx], dim=0)
+
     # print("index time: ", time.time() - start)
     return index_matrix
 
